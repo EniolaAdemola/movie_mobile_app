1a361d56e19399b088ccecdf3db1db816659728e
EniolaAdemola <it.eniolaademola@gmail.com>
2025-06-14 12:21:19 +0100

    feat: create initial index component for the Daveworld Movie app

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-accent text-3xl font-bold">
+        {" "}
+        Daveworld Movie app
+      </Text>
+    </View>
+  );
+}
