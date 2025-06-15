315d0aaf74d3ea6b5f82581efe581f2d6bf77b53
EniolaAdemola <it.eniolaademola@gmail.com>
2025-06-15 12:39:26 +0100

    feat: enhance Index component with SearchBar and movie fetching logic

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,83 @@
-import { Text, View } from "react-native";
+import SearchBar from "@/components/SearchBar_temp";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { useRouter } from "expo-router";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
 
 export default function Index() {
+  const router = useRouter();
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: movieError,
+  } = useFetch(() => fetchMovies({ query: "" }));
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-accent text-3xl font-bold">
-        {" "}
-        Daveworld Movie app
-      </Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+        {moviesLoading ? (
+          <ActivityIndicator size="large" color="#ab8bff" className="mt-10" />
+        ) : movieError ? (
+          <Text className="text-red-500 text-center mt-10">
+            {movieError.message}
+          </Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={() => router.push("/(tabs)/search")}
+              placeholder="Search for movies, titles and more..."
+              className="bg-dark-100 rounded-full"
+            />
+            <>
+              <Text className="text-lg text-white font-bold mt-5 mb-3">
+                {" "}
+                Latest movies
+              </Text>
+              <FlatList
+                data={movies}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => (
+                  <View
+                    className="bg-dark-200 rounded-lg p-4 mb-4"
+                    // onTouchEnd={() => router.push(`/movie/${item.id}`)}
+                  >
+                    <Image
+                      source={{
+                        uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+                      }}
+                      className="w-full h-48 rounded-lg mb-2"
+                      resizeMode="cover"
+                    />
+                    <Text className="text-white text-lg font-semibold">
+                      {item.title}
+                    </Text>
+                    <Text className="text-gray-400 text-sm mt-1">
+                      {item.release_date}
+                    </Text>
+                  </View>
+                )}
+                showsVerticalScrollIndicator={false}
+              />
+            </>
+          </View>
+        )}
+      </ScrollView>
     </View>
   );
 }
